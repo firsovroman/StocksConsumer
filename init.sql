67d266bd906a84657c4a4730e49d69d04f5ef5fb
Roman Firsov <firsov_r@stdpr.ru>
2023-09-21 11:30:42 +0800

    .

---
+++ init.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE apperate;
+CREATE USER srv_iexcloud WITH PASSWORD 'srv_iexcloud';
+GRANT ALL PRIVILEGES ON DATABASE apperate TO srv_iexcloud;
+
+-- DROP DATABASE apperate;
+-- DROP USER srv_iexcloud;
